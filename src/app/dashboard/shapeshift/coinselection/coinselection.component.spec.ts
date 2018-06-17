bb745af5e01d53eea148b2c043b723740e2d502c
tasos kakouris <tasoskakouris@gmail.com>
2018-06-18 01:40:59 +0300

    Shapeshift Select Coin Modal

--- src/app/dashboard/shapeshift/coinselection/coinselection.component.spec.ts
+++ src/app/dashboard/shapeshift/coinselection/coinselection.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CoinselectionComponent } from './coinselection.component';
+import { CoinSelectionComponent } from './coinselection.component';
 
 describe('CoinselectionComponent', () => {
-  let component: CoinselectionComponent;
-  let fixture: ComponentFixture<CoinselectionComponent>;
+  let component: CoinSelectionComponent;
+  let fixture: ComponentFixture<CoinSelectionComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CoinselectionComponent ]
+      declarations: [ CoinSelectionComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CoinselectionComponent);
+    fixture = TestBed.createComponent(CoinSelectionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
